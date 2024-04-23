5a9471d5012afadcbf4e3641841aa95508486ff9
Bieelogrom <gabrielamorim15436@gmail.com>
2024-04-22 23:34:00 -0300

    ATT

--- script.js
+++ script.js
@@ -111,3 +111,199 @@ function vinteCinco_7M(){
     let C = parseInt(prompt("Digite o valor de B:"))
     alert(`Resultado : ${(A + B + C) ** 2}`)
 }
+
+function vinteCinco_310A(){
+    let a = parseInt(prompt("Digite o valor de A: "))
+    let b = parseInt(prompt("Digite o valor de B: "))
+    let c = parseInt(prompt("Digite o valor de C: "))
+    let d = parseInt(prompt("Digite o valor de D: "))
+
+    let p = a * c
+    let s = b + d
+
+    alert(`Produto de A com C = ${p}\nSoma de B com D = ${s}`)
+}
+
+function vinteCinco_310B(){
+    let SM = parseFloat(prompt("Digite quanto você recebe por mês: "))
+    let PR = parseFloat(prompt("Digite o percentual de reajuste do salário: "))
+
+    let NS = SM + ((PR/100) * SM)
+
+    alert(`Seu saláro mensal: ${SM}R$\nPercentual de reajuste do salário: ${PR}%\nSalário após reajuste: ${NS}R$`)
+}
+
+function vinteCinco_310C(){
+    let votos_validos = parseInt(prompt("Digite a quantidade de votos válidos: "))
+    let votos_nulos = parseInt(prompt("Digite a quantidade de votos válidos: "))
+    let votos_brancos = parseInt(prompt("Digite a quantidade de votos válidos: "))
+
+    let quantidade_total_votos = votos_brancos + votos_nulos + votos_validos
+
+    alert(`Total de votos : ${quantidade_total_votos}\nVotos validos : ${(votos_validos/100) * quantidade_total_votos}%\nVotos nulos : ${(votos_nulos/100) * quantidade_total_votos}%\nVotos brancos : ${(votos_brancos / 100) * quantidade_total_votos}%`)
+}
+
+function quarentaUm_4A(){
+    let valor_um = parseInt(prompt("Digite o primeiro valor: "))
+    let valor_dois = parseInt(prompt("Digite o segundo valor: "))
+
+    let resposta = (valor_um > valor_dois) ? `Maior valor = ${valor_um}\nMenor valor = ${valor_dois}\nDiferença = ${valor_um - valor_dois}` : `Maior valor = ${valor_dois}\nMenor valor = ${valor_um}\nDiferença = ${valor_dois - valor_um}`
+
+    alert(resposta)
+}
+
+function quarentaUm_4B(){
+    let valor = parseFloat(prompt("Digite o valor desejado: "))
+    let resposta = (valor < 0) ? `O módulo de ${valor} é ${valor * -1}`: `O módulo de ${valor} é ${valor * -1}`
+    alert(resposta)
+}
+
+function quarentaUm_4C(){
+    let nota1 = parseFloat(prompt("Digite a primeira nota: "))
+    let nota2 = parseFloat(prompt("Digite a segunda nota: "))
+    let nota3 = parseFloat(prompt("Digite a terceira nota: "))
+    let nota4 = parseFloat(prompt("Digite a quarta nota: "))
+    let media = (nota1 + nota2 + nota3 + nota4) / 4
+    let resposta = (media >= 5) ? `Você foi aprovado!\nMédia = ${media}` : `Você foi reprovado\nMédia = ${media}`
+    alert(resposta)
+}
+
+function quarentaUm_4D(){
+    let nota1 = parseFloat(prompt("Digite a primeira nota: "))
+    let nota2 = parseFloat(prompt("Digite a segunda nota: "))
+    let nota3 = parseFloat(prompt("Digite a terceira nota: "))
+    let nota4 = parseFloat(prompt("Digite a quarta nota: "))
+    let media = (nota1 + nota2 + nota3 + nota4) / 4
+
+    if(media >= 7){
+        alert(`Você foi aprovado!\nSua média final : ${media}`)
+    }else{  
+        let nota_exame = parseFloat(prompt("Recuperação!\nDigite a nota do exame:"))
+        let nova_media = (nota_exame + media) / 2
+        if(nova_media >= 5){
+            alert(`Você foi aprovado no exame!\nSua média final : ${nova_media}`)
+        }else{
+            alert(`Você foi reprovado no exame!\nSua média final : ${nova_media}`)
+        }
+    }
+}
+
+function quarentaUm_4E(){
+    var valorA = parseFloat(prompt("Digite o valor de A:"));
+    var valorB = parseFloat(prompt("Digite o valor de B:"));
+    var valorC = parseFloat(prompt("Digite o valor de C:"));
+
+        if (valorA === 0) {
+            console.log("A variável 'a' deve ser diferente de zero.");
+            return;
+        }
+    
+        var delta = valorB * valorB - 4 * valorA * valorC;
+
+        if (delta < 0) {
+            console.log("Não há raízes reais para os valores informados.");
+            return;
+        }
+    
+        var x1 = (-valorB + Math.sqrt(delta)) / (2 * valorA);
+        var x2 = (-valorB - Math.sqrt(delta)) / (2 * valorA);
+    
+        console.log("As raízes da equação são:");
+        console.log("x1 =", x1);
+        console.log("x2 =", x2);
+}
+
+function quarentaUm_4F(){
+    let A = parseInt(prompt("Digite o valor de A"))
+    let B = parseInt(prompt("Digite o valor de B"))
+    let C = parseInt(prompt("Digite o valor de C"))
+
+    if(A > B && A > C){
+        if(B > C){
+            alert(`${C}, ${B}, ${A}`)
+        }else{
+            alert(`${B}, ${C}, ${A}`)
+        }
+    }else if(B > A && B > C){
+        if(A > C){
+            alert(`${C}, ${A}, ${B}`)
+        }else{
+            alert(`${A}, ${C}, ${B}`)
+        }
+    }else{
+        if(B > A){
+            alert(`${A}, ${B}, ${C}`)
+        }else{
+            alert(`${B}, ${A}, ${C}`)
+        }
+    }
+}
+
+function quarentaUm_4G(){
+    let valor = parseInt(prompt("Digite o valor: "))
+
+    if((valor % 3) == 0){
+        alert(`${valor} divide por três!`)
+    }else if((valor % 2) == 0){
+        alert(`${valor} divide por dois!`)
+    }else{
+        alert(`${valor} não divide por nenhum dos dois!`)
+    }
+}
+
+function quarentaUm_4H(){
+    let valores = []
+
+    for(let i = 0; i < 5; i++){
+        let numero = parseInt(prompt("digite o valor: "))
+        valores.push(numero)
+    }
+
+    let maior = valores[0]
+    let menor = valores[0]
+
+    for(let contador = 1; contador < valores.length; contador++){
+        if(valores[contador] > maior){
+            maior = valores[contador]
+        }
+        if(valores[contador] < menor){
+            menor = valores[contador]
+        }
+    }
+
+    alert(`menor valor = ${menor}\nMaior valor = ${maior}`)
+  
+}
+
+function quarentaUm_4I(){
+    let valor = parseInt(prompt("Digite o valor: "))
+
+    if((valor % 2) == 0){
+        alert("Valor é par")
+    }else{
+        alert("Valor é impar")
+    }
+}
+
+function quarentaUm_4J(){
+    let valor = parseInt(prompt("Digite o valor: "))
+    let resposta = (valor > 1 && valor < 9) ? "O valor está na faixa permitida!" : "O valor está fora da faixa permitida!"
+    alert(resposta)
+}
+
+function quarentaUm_4K(){
+    let valor = parseInt(prompt("Digite o valor"))
+    let resposta = (valor > 3) ? `ERRO` : `${valor}`
+    alert(resposta)
+}
+
+function quarentaUm_4L(){
+    let nome = prompt("Digite o nome: ")
+    let sexo = prompt("Digite o seu sexo\nM - Masculino ou F - Feminino")
+
+    if(sexo == "M" || sexo == "m"){
+        alert(`Llmo Sr. ${nome}`)
+    }else if(sexo == "F" || sexo == "f"){
+        alert(`Llmo Sra. ${nome}`)
+    }
+}
